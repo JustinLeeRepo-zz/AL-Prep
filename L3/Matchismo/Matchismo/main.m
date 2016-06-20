693bc3e1971529fa7a0fd403d6ffa432c5988c79
Justin Lee <justinj.lee23@gmail.com>
2016-06-20 00:27:21 -0700

    Lecture 3
    go over HW1 soln
    Create UI / view with 12 cards
    set playingCard model's score property
    then create algorithm for point logic within cardMatchingGame
    cardMatchingGame chooseCardAtIndex when button on view pressed

---
+++ L3/Matchismo/Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Justin Lee on 6/17/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+	}
+}
