4c3fc927636645d15577bc1906e1c46db045ba91
Justin Lee <justinj.lee23@gmail.com>
2016-06-21 15:09:13 -0700

    L5 View
    Add buttons to outline & unoutline
    Add buttons to change text color
    Add outlet connection for text color change buttons
    Add label as headerlabel w/ preferred headline font
    Add textview as body text w/ preferred body font

--- L5/Attributor/Attributor/AttributorViewController.m
+++ L5/Attributor/Attributor/AttributorViewController.m
@@ -9,19 +9,20 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 
 @end
 
 @implementation AttributorViewController
 
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+	[self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
+}
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
 }
 
 @end
