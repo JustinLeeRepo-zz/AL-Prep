209c5085e4c15ed4ac121c74ec156e2a00be0b00
Justin Lee <justinj.lee23@gmail.com>
2016-06-24 10:46:57 -0700

    L7 Demo
    Start of demo

---
+++ L7/SuperCard/SuperCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Justin Lee on 6/24/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L7/SuperCard/SuperCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Justin Lee on 6/24/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
