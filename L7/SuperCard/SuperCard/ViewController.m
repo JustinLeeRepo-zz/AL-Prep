dafb5106883eb82f376e3907388c0f082939d2ca
Justin Lee <justinj.lee23@gmail.com>
2016-06-24 12:13:58 -0700

    L7 Demo
    pinchGesture added programatically in playingCardView
    multiply scale factor by the scale of our pinch
    reset pinch scale to 1.0 so it is incremental, not cumulative
    
    add gestureRecognizer to VC w/ self.playingCardView as target
    and action as programmatically added pinch action

--- L7/SuperCard/SuperCard/ViewController.m
+++ L7/SuperCard/SuperCard/ViewController.m
@@ -25,6 +25,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
 	self.playingCardView.suit = @"♥";
 	self.playingCardView.rank = 13;
+	
+	[self.playingCardView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:self.playingCardView action:@selector(pinch:)]]; 
 }
 
 @end
