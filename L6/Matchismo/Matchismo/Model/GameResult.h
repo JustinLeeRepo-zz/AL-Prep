adbf78188c26d5ec49244051ee5518000557bf09
Justin Lee <justinj.lee23@gmail.com>
2016-06-25 15:16:23 -0700

    Assignment 3 Extra Credit 2
    Track user's high scores
    Add new model to keep track of scores, GameResult

---
+++ L6/Matchismo/Matchismo/Model/GameResult.h
@@ -0,0 +1,13 @@
+//
+//  GameResult.h
+//  Matchismo
+//
+//  Created by Justin Lee on 6/25/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResult : NSObject
+
+@end
---
+++ L6/Matchismo/Matchismo/Model/GameResult.m
@@ -0,0 +1,13 @@
+//
+//  GameResult.m
+//  Matchismo
+//
+//  Created by Justin Lee on 6/25/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import "GameResult.h"
+
+@implementation GameResult
+
+@end
