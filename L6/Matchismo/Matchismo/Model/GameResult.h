3e9c6039382280c5b272fc47e3a6acc8e7478d0b
Justin Lee <justinj.lee23@gmail.com>
2016-06-25 15:20:28 -0700

    Assignment 3 Extra Credit 2
    Create public interface for gameResult model
    w/ start, end, duration, score, & type properties

--- L6/Matchismo/Matchismo/Model/GameResult.h
+++ L6/Matchismo/Matchismo/Model/GameResult.h
@@ -10,4 +10,10 @@
 
 @interface GameResult : NSObject
 
+@property (nonatomic, readonly) NSDate *start;
+@property (nonatomic, readonly) NSDate *end;
+@property (nonatomic, readonly) NSTimeInterval duration;
+@property (nonatomic) int score;
+@property (nonatomic, strong) NSString *gameType;
+
 @end
