77da129adccea4e270fb591269eaaad7e5302519
Justin Lee <justinj.lee23@gmail.com>
2016-06-27 21:10:52 -0700

    L8 Demo
    blank proj

---
+++ L8/Dropit/Dropit/DropitAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Justin Lee on 6/27/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L8/Dropit/Dropit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dropit
+//
+//  Created by Justin Lee on 6/27/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
