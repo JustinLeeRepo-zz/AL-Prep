6d29b1d2e2a164d20a96e4fde5d994aa8af6410e
Justin Lee <justinj.lee23@gmail.com>
2016-06-27 21:56:06 -0700

    L8 Demo subclass UIDynamicBehavior
    DropitBehavior subclass of UIDynamicBehavior
    group gravity and collider behaviors into this subclass
    API methods to add and remove items from behaviors

---
+++ L8/Dropit/Dropit/DropitBehavior.m
@@ -0,0 +1,13 @@
+//
+//  DropitBehavior.m
+//  Dropit
+//
+//  Created by Justin Lee on 6/27/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import "DropitBehavior.h"
+
+@implementation DropitBehavior
+
+@end
