b4d19dce9fc1d6e022ea091f597bb479f29ef983
Justin Lee <justinj.lee23@gmail.com>
2016-06-29 15:12:31 -0700

    Assignment 3 Extra Credit 4
    Add new Model class GameSettings

---
+++ L6/Matchismo/Matchismo/Model/GameSettings.h
@@ -0,0 +1,13 @@
+//
+//  GameSettings.h
+//  Matchismo
+//
+//  Created by Justin Lee on 6/29/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameSettings : NSObject
+
+@end
---
+++ L6/Matchismo/Matchismo/Model/GameSettings.m
@@ -0,0 +1,13 @@
+//
+//  GameSettings.m
+//  Matchismo
+//
+//  Created by Justin Lee on 6/29/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import "GameSettings.h"
+
+@implementation GameSettings
+
+@end
