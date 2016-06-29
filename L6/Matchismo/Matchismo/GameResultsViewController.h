e88274151cf8d2c369b0a1bb19eaa85282acd131
Justin Lee <justinj.lee23@gmail.com>
2016-06-29 14:04:01 -0700

    Assignment 3 Extra Credit 2
    Results tab is now displaying text

---
+++ L6/Matchismo/Matchismo/GameResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameResultsViewController.h
+//  Matchismo
+//
+//  Created by Justin Lee on 6/29/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameResultsViewController : UIViewController
+
+@end
