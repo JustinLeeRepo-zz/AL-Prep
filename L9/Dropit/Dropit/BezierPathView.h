cbba0c5f7102dcab14fca1e845ac62063140b0a0
Justin Lee <justinj.lee23@gmail.com>
2016-06-30 23:31:37 -0700

    L9 Demo
    Add BezierPathView subclass of UIView
    Will be line between anchor point and view

---
+++ L9/Dropit/Dropit/BezierPathView.h
@@ -0,0 +1,13 @@
+//
+//  BezierPathView.h
+//  Dropit
+//
+//  Created by Justin Lee on 6/30/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BezierPathView : UIView
+
+@end
---
+++ L9/Dropit/Dropit/BezierPathView.m
@@ -0,0 +1,21 @@
+//
+//  BezierPathView.m
+//  Dropit
+//
+//  Created by Justin Lee on 6/30/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import "BezierPathView.h"
+
+@implementation BezierPathView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
