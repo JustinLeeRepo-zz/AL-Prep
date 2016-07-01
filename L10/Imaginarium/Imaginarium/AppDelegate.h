6fa06f59172a1914819f4ff7203357df1edf5b60
Justin Lee <justinj.lee23@gmail.com>
2016-07-01 15:45:47 -0700

    L10 Demo
    ScrolView, multithreading demo for Imaginarium

---
+++ L10/Imaginarium/Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by Justin Lee on 7/1/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L10/Imaginarium/Imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by Justin Lee on 7/1/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
