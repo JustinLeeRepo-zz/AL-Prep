bd430fff7466a4140c75f147f3d7b60e50740350
Justin Lee <justinj.lee23@gmail.com>
2016-07-01 08:58:51 -0700

    L9 AutoLayout Demo
    Copied Attributor demo from end of L6

---
+++ L9/Attributor/Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Justin Lee on 6/22/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
