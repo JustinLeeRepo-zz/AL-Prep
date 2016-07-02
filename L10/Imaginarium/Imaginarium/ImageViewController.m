dc13c4edbe930fd0a1d1a9caeccdf323722ab273
Justin Lee <justinj.lee23@gmail.com>
2016-07-01 17:16:33 -0700

    L10 Demo
    Create method to download img on separate thread
    Leave main thread available and responsive UI
    Session task creates new thread to download there

--- L10/Imaginarium/Imaginarium/ImageViewController.m
+++ L10/Imaginarium/Imaginarium/ImageViewController.m
@@ -36,7 +36,38 @@
 {
 	_imageURL = imageURL;
 	//dataWithContentsOfURL WILL BLOCK MAIN QUEUE
-	self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+//	self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+	[self startDownloadingImage];
+}
+
+- (void)startDownloadingImage
+{
+	self.image = nil;
+	if (self.imageURL) {
+		NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+		NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+		NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+		NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request
+														completionHandler:^(NSURL * _Nullable localfile, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+															if (!error) {
+																if ([request.URL isEqual:self.imageURL]) {
+																	//remote retrieval of data(internet) for above setImageURL
+																	//local retrieval of data so no blocking
+																	
+																	UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+																	
+																	dispatch_async(dispatch_get_main_queue(), ^{
+																		//this must be on main thread cause lots of UI manipulation
+																		self.image = image;
+																	});
+																	//identical to above block
+																	//[self performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:NO];
+																	
+																}
+															}
+														}];
+		[task resume];
+	}
 }
 
 - (UIImageView *)imageView
